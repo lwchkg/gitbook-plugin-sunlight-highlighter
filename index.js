d08955b6b640d95625c86f35e38d4a2bbc8f15a9
WC Leung <lwchkg@gmail.com>
2017-06-25 16:41:10 +0000

    Add line numbering, theme support, update to ES6.

--- index.js
+++ index.js
@@ -1,53 +1,103 @@
-var sunlight = require("./sunlight-all-min.js").Sunlight;
-var highlighter = new sunlight.Highlighter();
+'use strict';
 
+const fse = require('fs-extra');
+const path = require('path');
+const sunlight = require('./sunlight-all-min.js').Sunlight;
+// const highlighter = new sunlight.Highlighter();
+
+const cssFile = 'sunlight.css';
+const pluginName = 'sunlight-highlighter';
+
+let defaultLineNumber;
+
+/**
+ * Validate the theme. If invalid output 'default' instead of the input string.
+ * @param {string} theme The theme to be validated.
+ * @returns {string} The valudated theme.
+ */
+function ValidateTheme(theme) {
+  return theme === 'dark' ? 'dark' : 'default';
+}
+
+/**
+ * Write assets
+ * @param {Object} output output object by GitBook.
+ * @param {Object} log log object by GitBook.
+ * @param {string} theme The theme selected by the options.
+ */
+function WriteAssets(output, log, theme) {
+  const pluginPath = path.join('gitbook', `gitbook-plugin-${pluginName}`);
+
+  const inputFile = path.join(__dirname, 'themes', `sunlight.${theme}.css`);
+  const outputPath = path.join(output.root(), pluginPath);
+  const outputFile = path.join(outputPath, cssFile);
+  log.debug.ln('Writing the theme...');
+  fse.ensureDirSync(outputPath);
+  fse.copySync(inputFile, outputFile, {preserveTimestamps: true});
+}
+
+/**
+ * Loads the book info.
+ * @param {Object} options Pass this.options to this parameter.
+ * @param {Object} log log object by GitBook.
+ * @param {Object} output
+ */
+function loadBookInfo(options, log, output) {
+  const pluginOptions = options.pluginsConfig['sunlight-highlighter'];
+
+  const theme = ValidateTheme(pluginOptions.theme);
+  WriteAssets(output, log, theme);
+  defaultLineNumber = pluginOptions.lineNumber;
+}
+
+/**
+ * Highlight the code block.
+ * @param {string} lang The language and other parameter.
+ * @param {string} code The code to be highlighted.
+ * @returns {string} The highlighted HTML code.
+ */
 function highlight(lang, code) {
-    if(!lang) return {
-        body: code,
-        html: false
-    };
+  if (!lang) return {
+    body: code,
+    html: false,
+  };
 
-    try {
-      //first argument is the text to highlight, second is the language id
-      var context = highlighter.highlight(code, lang);
-      var nodes = context.getNodes(); //array of DOM nodes
+  try {
+    const lineNumbers = defaultLineNumber !== false;
+    const highlighter = new sunlight.Highlighter({lineNumbers: lineNumbers});
 
-      //the following will convert it to an HTML string
-      var jsdom = require("jsdom").jsdom;
-      var document = jsdom("", {});
+    const jsdom = require('jsdom').jsdom;
+    const document = jsdom('', {});
 
-      var dummyElement = document.createElement("div");
-      for (var i = 0; i < nodes.length; i++) {
-          dummyElement.appendChild(nodes[i]);
-      }
+    const preElement = document.createElement('div');
+    // Note: setting innerText does not work in jsdoc 9.4.2
+    preElement.appendChild(document.createTextNode(code));
+    preElement.setAttribute('class', `sunlight-highlight-${lang}`);
 
-      var rawHtml = dummyElement.innerHTML;
-      return rawHtml;
-    } catch(e) { console.log(e); }
+    const dummyElement = document.createElement('div');
+    dummyElement.appendChild(preElement);
+    highlighter.highlightNode(preElement);
 
-    return {
-        body: code,
-        html: false
-    };
+    return dummyElement.innerHTML;
+  } catch (e) { console.log(e); }
+
+  return {
+    body: code,
+    html: false,
+  };
 }
 
 module.exports = {
-    book: {
-        assets: "./themes",
-        js: [],
-        css: [
-            "sunlight.default.css"
-        ]
+  book: {css: [cssFile]},
+  ebook: {css: [cssFile]},
+  blocks: {
+    code: function(block) {
+      return highlight(block.kwargs.language, block.body);
     },
-    ebook: {
-        assets: "./themes",
-        css: [
-            "sunlight.default.css"
-        ]
+  },
+  hooks: {
+    init: function() {
+      loadBookInfo(this.options, this.log, this.output);
     },
-    blocks: {
-        code: function(block) {
-            return highlight(block.kwargs.language, block.body);
-        }
-    }
+  },
 };
